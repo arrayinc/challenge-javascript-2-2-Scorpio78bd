eb38b1c86dab684b149e8a25a5d819236d0a9099
Barbara Deines <barbaradeines@Barbaras-MacBook.local>
2018-12-05 15:57:51 -0700

    real answer to challenge 2 after reading through directions

--- answer.js
+++ answer.js
@@ -1,7 +1,9 @@
 // *** YOUR ANSWER BELOW ***
 function quotient(x, y){
-  let positive = Math.floor (x / y);
-  return positive;
+  let challenge = (x % y);
+  let changed = x - challenge;
+  let answer = changed / y;
+  return answer;
 }
 
 quotient(9,4)
